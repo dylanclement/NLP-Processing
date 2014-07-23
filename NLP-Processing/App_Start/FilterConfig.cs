4f2a7924669003bbe1eda2f9e90561996e7a8c36
Dylan Clement <dylan.clement@adslot.com>
2014-07-23 22:42:14 +1000

    Initial check in

---
+++ NLP-Processing/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NLP_Processing
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NLP-Processing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NLP_Processing.WebApiApplication" Language="C#" %>
