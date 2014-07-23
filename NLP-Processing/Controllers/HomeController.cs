16729a785dabfd6810ae1003695414b951b6cfa1
Dylan Clement <dylanclement@gmail.com>
2014-07-23 23:49:09 +1000

    Add stanford NLP tagger

--- NLP-Processing/Controllers/HomeController.cs
+++ NLP-Processing/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using edu.stanford.nlp.parser;
+using edu.stanford.nlp.tagger.maxent;
 
 namespace NLP_Processing.Controllers
 {
@@ -10,7 +12,21 @@ namespace NLP_Processing.Controllers
     {
         public ActionResult Index()
         {
+            var tagger = new MaxentTagger(@"C:\Projects\NLP-Processing\stanford-postagger-full-2014-06-16\models\wsj-0-18-bidirectional-nodistsim.tagger");
+            var sentences = MaxentTagger.tokenizeText(new java.io.StringReader(@"A cat is an animal")).toArray();
+            //sentences = new java.util.ArrayList(sentences);
+            foreach(var s in sentences)
+            {
+                var taggedSentence = tagger.tagSentence((java.util.ArrayList)s).toArray();
+                var results = "";
+                foreach (var tag in taggedSentence)
+                {
+                    results += tag.ToString() + "\n";
+                }
+                ViewBag.results += results;
+            };
             ViewBag.Title = "Home Page";
+            
 
             return View();
         }
